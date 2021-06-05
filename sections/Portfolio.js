c1d2ee9e09aa9259b738492d276932ea8bee0333
Jabez Sanjay <jbzsanjay2000@gmail.com>
2021-06-05 21:05:30 +0530

    Basic Portfolio-v2 Setup Completed

---
+++ sections/Portfolio.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Portfolio = () => {
+  return (
+    <div className="sections">
+      <h1>Portfolio</h1>
+    </div>
+  );
+};
+
+export default Portfolio;
---
+++ sections/Work.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Work = () => {
+  return (
+    <div className="sections">
+      <h1>Work</h1>
+    </div>
+  );
+};
+
+export default Work;
