302b56a9521263cf2f92e9f3c4ef0259c41afd85
Jabez Sanjay <jbzsanjay2000@gmail.com>
2021-06-13 11:40:18 +0530

    Added new home-svg

--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,10 @@ import Portfolio from "../sections/Portfolio";
 import Work from "../sections/Work";
 import Testimonials from "../sections/Testimonials";
 import Services from "../sections/Services";
-import Navbar from "../components/Navbar";
 
 const index = () => {
   return (
     <>
-      <Navbar />
       <Home />
       <About />
       <Services />
--- sections/Home.js
+++ sections/Home.js
@@ -4,11 +4,13 @@ var Scroll = require("react-scroll");
 var Element = Scroll.Element;
 import Image from "next/image";
 import COLORS from "../assets/colors";
+import Navbar from "../components/Navbar";
 
 const Home = () => {
   return (
     <HomeTag>
       <Element name="home">
+        <Navbar />
         <section className="homepage">
           <div className="homepage__container">
             <div className="homepage__split">
@@ -37,23 +39,23 @@ export default Home;
 
 const HomeTag = styled.div`
   .homepage {
-    background-image: url("/Images/bg-main.jpg");
+    background-image: url("/Images/home-wave.svg");
     background-repeat: no-repeat;
     background-size: cover;
     background-position: center center;
     display: flex;
     justify-content: center;
     align-items: center;
+    min-height: 90vh;
     &__container {
       margin-inline: auto;
-      width: min(90%, 90rem);
+      width: min(90%, 74rem);
     }
     &__split {
       display: flex;
       flex-direction: column;
       justify-content: center;
       align-items: center;
-      margin-bottom: 5rem;
       h1 {
         text-align: center;
         font-size: 1.7rem;
